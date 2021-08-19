106d7ed9288ceb1109b6e852123fa21af52e76a0
Fernando74lr <flopezramirez@hotmail.com>
2021-08-19 15:46:12 +0000

    Renamed files

--- hello_world/hello_world.js
+++
@@ -1,8 +0,0 @@
-
-const greeting = () => {
-    let hour = new Date().getHours();
-    if (hour < 12) alert('Good morning 👋');
-    if (hour >= 12 && hour < 18) alert('Good afternoon 👋');
-    if (hour >= 18 && hour < 20) alert('Good evening 🌗');
-    if (hour >= 20) alert('Good night 🌙');
-}
--- hello_world/hellow_world.js
+++
@@ -1,8 +0,0 @@
-
-const greeting = () => {
-    let hour = new Date().getHours();
-    if (hour < 12) console.log('Good morning 👋');
-    if (hour >= 12 && hour < 18) console.log('Good afternoon 👋');
-    if (hour >= 18 && hour < 20) console.log('Good evening 🌗');
-    if (hour >= 20) console.log('Good night 🌙');
-}
